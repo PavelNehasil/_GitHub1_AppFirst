0e80329397a945a4744f774de8e0148ebeacd52b
Pavel Nehasil <77880602+PavelNehasil@users.noreply.github.com>
2025-01-08 17:11:11 +0100

    image

---
+++ AppFirst/AppFirst/Models/LoginImage.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace AppFirst.Models
+{
+    [Table("LoginImages")]
+    public class LoginImage : INotifyPropertyChanged
+    {
+        private int id;
+        private string imageName;
+        private string description;
+        private byte[] image;
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id
+        {
+            get => id;
+            set => SetProperty(ref id, value);
+        }
+
+        [Required]
+        [StringLength(250)]
+        public string ImageName
+        {
+            get => imageName;
+            set => SetProperty(ref imageName, value);
+        }
+
+        [StringLength(500)]
+        public string Description
+        {
+            get => description;
+            set => SetProperty(ref description, value);
+        }
+
+        public byte[] Image
+        {
+            get => image;
+            set => SetProperty(ref image, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Id == (obj as LoginImage).Id;
+        }
+    }
+}
---
+++ AppFirst/AppFirst/Services/LoadSqlDataSqliteService_LoginImage.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.ObjectModel;
+using System.Data.SQLite;
+using AppFirst.Models;
+
+namespace AppFirst.Services
+{
+    public class LoadSqlDataSqliteService_LoginImage
+    {
+        private readonly string _connectionString;
+
+        public LoadSqlDataSqliteService_LoginImage(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task GetLoginImages(ObservableCollection<LoginImage> loginImages)
+        {
+            loginImages.Clear();
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var command = new SQLiteCommand("""
+                    SELECT Id, ImageName, Description, Image FROM LoginImages
+                    ORDER BY Id
+                    """, connection);
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        var loginImage = new LoginImage
+                        {
+                            Id = reader.GetInt32(0),
+                            ImageName = reader.GetString(1),
+                            Description = reader.IsDBNull(2) ? null : reader.GetString(2),
+                            Image = reader.IsDBNull(3) ? null : (byte[])reader["Image"]
+                        };
+                        loginImages.Add(loginImage);
+                    }
+                }
+                await connection.CloseAsync();
+            }
+        }
+
+        public async Task DeleteLoginImageAsync(int idLoginImage)
+        {
+            if (idLoginImage < 1) return;
+
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var command = new SQLiteCommand("""
+                    DELETE FROM LoginImages
+                    WHERE Id = @id
+                    """, connection);
+
+                command.Parameters.AddWithValue("@id", idLoginImage);
+                await command.ExecuteNonQueryAsync();
+                await connection.CloseAsync();
+            }
+        }
+
+        public async Task InsertLoginImageAsync(int idLoginImage, string imageName, string description, byte[] image)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var command = new SQLiteCommand("""
+                    INSERT INTO LoginImages (Id, ImageName, Description, Image) VALUES (@id, @imageName, @description, @image)
+                    """, connection);
+                command.Parameters.AddWithValue("@id", idLoginImage);
+                command.Parameters.AddWithValue("@imageName", imageName);
+                command.Parameters.AddWithValue("@description", description);
+                command.Parameters.AddWithValue("@image", image);
+                await command.ExecuteNonQueryAsync();
+                await connection.CloseAsync();
+            }
+        }
+    }
+}
